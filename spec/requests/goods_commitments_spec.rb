1a6af3ac8ec5c0881bf7f12537a05022b359b3cb
Asif CH <asif.chelackoden@gmail.com>
2018-08-22 13:05:34 +0530

    - path error

--- spec/requests/goods_commitments_spec.rb
+++ spec/requests/goods_commitments_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "GoodsCommitments", type: :request do
   describe "GET /goods_commitments" do
     it "works! (now write some real specs)" do
-      get goods_commitments_path
+      get goods_commitment_path
       # expect(response).to have_http_status(200)
     end
   end
